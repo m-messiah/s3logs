273f32983e00c832d29f113229a6b825fea232d0
Maxim Muzafarov <m-messiah@yandex-team.ru>
2019-05-28 22:10:20 +0300

    Build for all branches, deploy only in tags

--- setup.py
+++ setup.py
@@ -17,26 +17,11 @@ setup(
     install_requires=["boto", 'filechunkio'],
     keywords='logs s3 nginx',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: System Administrators',
         'Topic :: Utilities',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 )
